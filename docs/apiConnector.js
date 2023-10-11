e499d5a058186e80cf96f9504d6518a509313450
Niklas Deeg <the_niklas@hotmail.de>
2023-10-11 23:36:43 +0200

    test123

--- docs/apiConnector.js
+++ docs/apiConnector.js
@@ -13,5 +13,22 @@
         console.log(response);
         return response;
       });
-  }
+}
+
+function postData(antworten) {
+    var settings = {
+        "url": "https://niklasdeegapi.azurewebsites.net/ProjectM",
+        "method": "POST",
+        "timeout": 0,
+        "headers": {
+            "Content-Type": "application/json",
+            "Cookie": "ARRAffinity=22a7daa836b64a8ce56c907737553d08297ff2e76cd06a1f52c29956b9a85c17; ARRAffinitySameSite=22a7daa836b64a8ce56c907737553d08297ff2e76cd06a1f52c29956b9a85c17"
+        },
+        "data": JSON.stringify(antworten),
+    };
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+}
   
