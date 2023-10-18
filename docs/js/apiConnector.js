a07fd4a527f9f00fae3b2a686d23828292286392
Niklas Deeg <the_niklas@hotmail.de>
2023-10-18 15:55:56 +0200

    Adminbereich + js-Änderungen

--- docs/js/apiConnector.js
+++ docs/js/apiConnector.js
@@ -1,26 +1,22 @@
 
 //Um die bereits abgespeicherten Antworten zu einer Email zu bekommen
-//Es wird die verifizierte Email (string) benötigt
+//Es wird die verifizierte Email (string) benï¿½tigt
 //In der Response sind die Antworten als Stringarray enthalten
-function getData(email)
-  {
-      var settings = {
-          "url": "https://niklasdeegapi.azurewebsites.net/ProjectM?email=" + email,
-          "method": "GET",
-          "timeout": 0,
-          "headers": {
-              "Cookie": "ARRAffinity=22a7daa836b64a8ce56c907737553d08297ff2e76cd06a1f52c29956b9a85c17; ARRAffinitySameSite=22a7daa836b64a8ce56c907737553d08297ff2e76cd06a1f52c29956b9a85c17"
-          },
-      };
+function getData(email) {
+    var settings = {
+        "url": "https://niklasdeegapi.azurewebsites.net/ProjectM?email=" + email,
+        "method": "GET",
+        "timeout": 0,
+        "headers": {
+            "Cookie": "ARRAffinity=22a7daa836b64a8ce56c907737553d08297ff2e76cd06a1f52c29956b9a85c17; ARRAffinitySameSite=22a7daa836b64a8ce56c907737553d08297ff2e76cd06a1f52c29956b9a85c17"
+        },
+    };
 
-      $.ajax(settings).done(function (response) {
-          console.log(response);
-          return response;
-      });
+    return settings;
 };
 
 //Zum Abspeichern der Antworten
-//Es wird die verifizierte Email (string) und die Antworten als Stringarray benötigt
+//Es wird die verifizierte Email (string) und die Antworten als Stringarray benï¿½tigt
 //Es wird die Antwort der API returned, diese kann dem User angezeigt werden
 function postData(email, antworten) {
 
@@ -38,14 +34,11 @@ function postData(email, antworten) {
         }),
     };
 
-    $.ajax(settings).done(function (response) {
-        console.log(response);
-        return response;
-    });
+    return settings;
 };
 
 //Zum erneuten Anfordern des Codes zum Verifizieren der Email
-//Hier muss die Email als Parameter zurückgegeben werden
+//Hier muss die Email als Parameter angegeben werden
 //Es wird die Antwort der API returned, diese kann dem User angezeigt werden
 function postEmailCodeAgain(email) {
     var settings = {
@@ -58,9 +51,79 @@ function postEmailCodeAgain(email) {
         "data": JSON.stringify(email),
     };
 
-    $.ajax(settings).done(function (response) {
-        console.log(response);
-        return response;
-    });
+    return settings;
 };
-  
+
+
+//LÃ¶scht alle Antworten in der DB
+//Es muss das entsprechende Passwort mitgegeben werden
+//Beim Erfolg wird "success" returned
+function deleteAllAnswers(password) {
+    var settings = {
+        "url": "https://niklasdeegapi.azurewebsites.net/ProjectM?password=" + password,
+        "method": "DELETE",
+        "timeout": 0,
+        "headers": {
+            "Cookie": "ARRAffinity=22a7daa836b64a8ce56c907737553d08297ff2e76cd06a1f52c29956b9a85c17; ARRAffinitySameSite=22a7daa836b64a8ce56c907737553d08297ff2e76cd06a1f52c29956b9a85c17"
+        },
+    };
+
+    return settings;
+};
+
+//Gibt alle Antworten aus der DB im Format eines Arrays zurÃ¼ck. Jeder Array-Eintrag sind die Antworten einer Person mit Semikolon getrennt (Also sieht das Array ungefÃ¤hr so aus: [{"Antwort1";"Antwort2";"usw"}, {"Antwort1";"Antwort2";"usw"}])
+//Es muss das entsprechende Passwort mitgegeben werden
+//Beim Erfolg wird "success" returned
+function getAllSurveys(password, error, success) {
+    var settings = {
+        "url": "https://niklasdeegapi.azurewebsites.net/ProjectM/GetAllSurveys?password=" + password,
+        "method": "GET",
+        "timeout": 0,
+        "headers": {
+            "Cookie": "ARRAffinity=22a7daa836b64a8ce56c907737553d08297ff2e76cd06a1f52c29956b9a85c17; ARRAffinitySameSite=22a7daa836b64a8ce56c907737553d08297ff2e76cd06a1f52c29956b9a85c17"
+        },
+    };
+
+    return settings;
+};
+
+
+//Zum Anfordern des Codes zum Verifizieren der Email
+//Hier muss die Email als Parameter angegeben werden
+//Es wird die Antwort der API returned, diese kann dem User angezeigt werden
+function postEmail(email) {
+    var settings = {
+        "url": "https://niklasdeegapi.azurewebsites.net/ProjectM/PostEmail",
+        "method": "POST",
+        "timeout": 0,
+        "headers": {
+            "Content-Type": "application/json",
+            "Cookie": "ARRAffinity=22a7daa836b64a8ce56c907737553d08297ff2e76cd06a1f52c29956b9a85c17; ARRAffinitySameSite=22a7daa836b64a8ce56c907737553d08297ff2e76cd06a1f52c29956b9a85c17"
+        },
+        "data": JSON.stringify(email),
+    };
+
+    return settings;
+};
+
+//Zum Verifizieren der Email
+//Hier muss die Email als Parameter angegeben werden, sowie der Code den man per Mail erhalten hat
+//Es wird die Antwort der API returned, diese kann dem User angezeigt werden
+function verifyEmail(email, code) {
+    var settings = {
+        "url": "https://niklasdeegapi.azurewebsites.net/ProjectM/VerifyEmail?email=" + email,
+        "method": "POST",
+        "timeout": 0,
+        "headers": {
+            "Content-Type": "application/json",
+            "Cookie": "ARRAffinity=22a7daa836b64a8ce56c907737553d08297ff2e76cd06a1f52c29956b9a85c17; ARRAffinitySameSite=22a7daa836b64a8ce56c907737553d08297ff2e76cd06a1f52c29956b9a85c17"
+        },
+        "data": JSON.stringify(code),
+    };
+
+    return settings;
+};
+
+function ajaxCall(settings) {
+    return $.ajax(settings);
+}
---
+++ docs/js/csvGenerator.js
@@ -0,0 +1,6 @@
+//Zum generieren einer CSV-Datei die alle Antworten beeinhaltet
+//Es müssen in Antworten angegeben werden im Format eines Arrays. Jeder Array-Eintrag sind die Antworten einer Person mit Semikolon getrennt (Also sieht das Array ungefähr so aus: [{"Antwort1";"Antwort2";"usw"}, {"Antwort1";"Antwort2";"usw"}])
+//
+function generateCSV(antworten) {
+    //ToDo
+};
