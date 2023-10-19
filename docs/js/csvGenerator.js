2319d8c47f54709276ff7e792abd6fc42c981d9b
Niklas Deeg <58047309+Niklas-Deeg@users.noreply.github.com>
2023-10-19 13:36:57 +0200

    Update csvGenerator.js

--- docs/js/csvGenerator.js
+++ docs/js/csvGenerator.js
@@ -4,8 +4,8 @@
 function generateCSV(antworten) {
     
     let csvContent = "data:text/csv;charset=utf-8,";
-        rows.forEach(function(rowArray) {
-            let row = rowArray.join(",");
+        antworten.forEach(function(rowArray) {
+            let row = rowArray;
         csvContent += row + "\r\n";
         });
     var encodedUri = encodeURI(csvContent);
