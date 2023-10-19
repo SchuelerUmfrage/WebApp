9bc91a5ec27ff734bfc5201c00e840b8ab4c0139
itsMolf121 <113800297+itsMolf121@users.noreply.github.com>
2023-10-19 10:43:14 +0200

    Update csvGenerator.js
    probably falsch, aber musste los

--- docs/js/csvGenerator.js
+++ docs/js/csvGenerator.js
@@ -2,5 +2,14 @@
 //Es müssen in Antworten angegeben werden im Format eines Arrays. Jeder Array-Eintrag sind die Antworten einer Person mit Semikolon getrennt (Also sieht das Array ungefähr so aus: [{"Antwort1";"Antwort2";"usw"}, {"Antwort1";"Antwort2";"usw"}])
 //
 function generateCSV(antworten) {
+    
+    let csvContent = "data:text/csv;charset=utf-8,";
+        rows.forEach(function(rowArray) {
+            let row = rowArray.join(",");
+        csvContent += row + "\r\n";
+        });
+    var encodedUri = encodeURI(csvContent);
+        window.open(encodedUri);
+    
     //ToDo
 };
